1ee911b511bae66ed66cf863fb7def27f6835e25
Alexandor <alexandor.savenok@gmail.com>
2016-12-26 14:49:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Properties property = new Properties();
+        InputStream inputFIle = null;
+
+        try {
+
+            inputFIle = new FileInputStream("src\\resources\\config.properties");
+            property.load(inputFIle);
+
+            System.out.println(property.getProperty("db.host"));
+            System.out.println(property.getProperty("db.login"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if(inputFIle != null) {
+                try {
+                    inputFIle.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
